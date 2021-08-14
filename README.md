190315f03e0b033d6f5e8d62376d5faf72fb5100
Nguyen Ngoc Linh <mail2ngoclinh@gmail.com>
2021-08-14 21:54:07 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+bingo
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/bingo_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc bingo public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(bingo_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    bingo_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
